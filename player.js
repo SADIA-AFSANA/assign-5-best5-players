41b9fd1b379afb5575762302a9a31f90cc5979d6
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-08-22 21:15:43 +0600

    five players selection done

--- player.js
+++ player.js
@@ -1,1 +1,29 @@
-console.log('hi');
+const array = [];
+
+
+function display(selectPlayers) {
+    console.log(selectPlayers);
+
+    const tableBody = document.getElementById('players-list');
+    tableBody.innerHTML = '';
+    for (let i = 0; i < selectPlayers.length; i++) {
+
+        // console.log(array[i].playersName);
+
+        const footballPlayer = array[i].playersName;
+
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${footballPlayer}</td>
+        
+        `;
+        if (selectPlayers.length > 5) {
+            alert('can not select other players');
+            return;
+        }
+
+        tableBody.appendChild(tr);
+    }
+
+}
