783d51a82ce212c447d0bd0a8b8d30d01fa156a0
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-08-22 21:20:57 +0600

    js writing done

--- player.js
+++ player.js
@@ -42,3 +42,42 @@ function addToPlayer(element) {
     document.getElementById('total-added-players').innerText = array.length;
     display(array);
 }
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const perPlayer = document.getElementById('per-player-input');
+    const perPlayerString = perPlayer.value;
+    const perPlayerRate = parseFloat(perPlayerString);
+    perPlayer.value = '';
+
+    const playerExpences = document.getElementById('player-Expenses');
+    const playerExpencesString = playerExpences.innerText;
+    const playerExpencesTotal = parseFloat(playerExpencesString);
+
+    const currentExpenceMoney = array.length * perPlayerRate;
+    playerExpences.innerText = currentExpenceMoney;
+
+})
+
+document.getElementById('calculate-total-btn').addEventListener('click', function () {
+
+    const managerExpences = document.getElementById('manager-input');
+    const managerExpencesString = managerExpences.value;
+    const managerExpenceTotal = parseFloat(managerExpencesString);
+    managerExpences.value = '';
+
+    const coachExpences = document.getElementById('coach-input');
+    const coachExpencesString = coachExpences.value;
+    const coachExpencesTotal = parseFloat(coachExpencesString);
+    coachExpences.value = '';
+
+    const calculateTotal = document.getElementById('final-total');
+    const calculateTotalString = calculateTotal.innerText;
+    const calculateTotalMoney = parseFloat(calculateTotalString);
+
+    const playerExpences = document.getElementById('player-Expenses');
+    const playerExpencesString = playerExpences.innerText;
+    const playerExpencesTotal = parseFloat(playerExpencesString);
+
+
+    const totalCost = playerExpencesTotal + coachExpencesTotal + managerExpenceTotal + calculateTotalMoney;
+    calculateTotal.innerText = totalCost;
+})
